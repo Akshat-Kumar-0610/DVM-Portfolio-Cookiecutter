9732048e28d67fb5d7dbd2b93d0019b3333c807b
cry0genic <cry0genic1103@gmail.com>
2021-06-09 12:44:41 +0530

    syncdb

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,7 @@ fi
 echo "Making migrations and migrating the database. "
 python manage.py makemigrations main --noinput 
 python manage.py migrate --noinput 
+python manage.py migrate --run-syncdb
 python manage.py collectstatic --noinput
 
 exec "$@"
